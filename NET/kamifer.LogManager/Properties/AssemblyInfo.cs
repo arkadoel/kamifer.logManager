f79b71a1cf45f9cfe87c53bb2266c70ac04dd2e4
Fernando David Minguela Pablos <Fernando David Minguela Pablos>
2016-08-03 12:14:49 +0200

    comiteando version libreria

--- NET/kamifer.LogManager/Properties/AssemblyInfo.cs
+++ NET/kamifer.LogManager/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("kamifer.LogManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("http://github.com/arkadoel")]
 [assembly: AssemblyProduct("kamifer.LogManager")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2016.08.03.0")]
+[assembly: AssemblyFileVersion("2016.08.03.0")]
